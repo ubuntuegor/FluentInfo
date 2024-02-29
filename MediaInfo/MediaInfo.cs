0fc94c21cfcbe92df17fe421886015c3f5d16a65
Egor Kukoverov <me@bnt.to>
2024-02-29 20:16:12 +0200

    MediaInfoLib: add Option

--- MediaInfo/MediaInfo.cs
+++ MediaInfo/MediaInfo.cs
@@ -10,6 +10,8 @@ namespace MediaInfoLib
         [DllImport("MediaInfo.dll")]
         private static extern void MediaInfo_Delete(IntPtr handle);
         [DllImport("MediaInfo.dll", CharSet = CharSet.Unicode)]
+        private static extern IntPtr MediaInfo_Option(IntPtr handle, string parameter, string value);
+        [DllImport("MediaInfo.dll", CharSet = CharSet.Unicode)]
         private static extern IntPtr MediaInfo_Open(IntPtr handle, string filename);
         [DllImport("MediaInfo.dll")]
         private static extern IntPtr MediaInfo_Inform(IntPtr handle, IntPtr reserved);
@@ -19,6 +21,11 @@ namespace MediaInfoLib
         public MediaInfo()
         {
             handle = MediaInfo_New();
+        }
+
+        public string? Option(string parameter, string value)
+        {
+            return Marshal.PtrToStringUni(MediaInfo_Option(handle, parameter, value));
         }
 
         // Returns true if opened successfully
